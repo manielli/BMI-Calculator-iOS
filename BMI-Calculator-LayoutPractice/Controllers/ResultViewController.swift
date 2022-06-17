39cc29a65758a02b356c06491c400daa06c31f05
Mani Jafari <contact@manijafari.com>
2022-06-17 10:33:26 -0700

    Create a new ResultViewController to be linked to the second ViewControler in Main storyboard, assign ResultViewController to the second ViewController in Main storyboard and add ResultViewController as its class in its identitiy inspector in inspector pane

---
+++ BMI-Calculator-LayoutPractice/Controllers/ResultViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ResultViewController.swift
+//  BMI-Calculator-LayoutPractice
+//
+//  Created by Mani-Jafari on 2022-06-17.
+//  Copyright © 2022 App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
