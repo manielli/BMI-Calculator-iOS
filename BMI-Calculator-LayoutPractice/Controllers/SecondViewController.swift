f940424dbdd4eb055fa17a7230aea91fef356fbb
Mani Jafari <contact@manijafari.com>
2022-06-17 10:02:52 -0700

    Create a new ViewController named SecondViewController, in Controllers folder, add viewDidLoad overriding function

---
+++ BMI-Calculator-LayoutPractice/Controllers/SecondViewController.swift
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.swift
+//  BMI-Calculator-LayoutPractice
+//
+//  Created by Mani-Jafari on 2022-06-17.
+//  Copyright © 2022 App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
