0f2e4d0140670302de6ef11ecbf15e08a03ea991
Mani Jafari <contact@manijafari.com>
2022-06-17 13:40:12 -0700

    Create a Models group with folder, and replace the Models group without folder, move CalculatorBrain from  Controllers folder to Models folder, create a BMI model file in Models folder group

---
+++ BMI-Calculator-LayoutPractice/Models/BMI.swift
@@ -0,0 +1,13 @@
+//
+//  BMI.swift
+//  BMI-Calculator-LayoutPractice
+//
+//  Created by Mani-Jafari on 2022-06-17.
+//  Copyright © 2022 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct BMI {
+    
+}
