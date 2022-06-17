5f663803644d9eb4ddd44bc8b918708d3d20f38f
Mani Jafari <contact@manijafari.com>
2022-06-17 11:30:31 -0700

    Create two interface builder outlets for the bmi label and the advice label on ResultViewController, create a new interface builder action for function recalculatePressed

--- BMI-Calculator-LayoutPractice/Controllers/ResultViewController.swift
+++ BMI-Calculator-LayoutPractice/Controllers/ResultViewController.swift
@@ -10,11 +10,18 @@ import UIKit
 
 class ResultViewController: UIViewController {
 
+    @IBOutlet weak var bmiLabel: UILabel!
+    @IBOutlet weak var adviceLabel: UILabel!
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
     
+
+    @IBAction func recalculatePressed(_ sender: UIButton) {
+        
+    }
 
     /*
     // MARK: - Navigation
